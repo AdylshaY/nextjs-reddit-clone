c06968e0c913b56e23dd5a3142e1eb8d4743c31d
Adylsha Yumayev <adylshay@gmail.com>
2023-02-03 10:46:54 +0300

    navbar auth buttons created

---
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -0,0 +1,19 @@
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+import AuthButton from './AuthButton';
+
+type RightContentProps = {
+  // user:
+};
+
+const RightContent: React.FC<RightContentProps> = () => {
+  return (
+    <>
+      {/* <AuthModal /> */}
+      <Flex justify='center' align='center'>
+        <AuthButton />
+      </Flex>
+    </>
+  );
+};
+export default RightContent;
